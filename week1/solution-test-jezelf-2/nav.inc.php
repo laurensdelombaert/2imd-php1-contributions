fb6eac30fa95f650d3e3dd94dfd3ddd802f3ec1b
Bibi <bibi.treffers@outlook.com>
2018-03-07 21:49:33 +0100

    Update nav.inc.php
    shorter version of code

--- week1/solution-test-jezelf-2/nav.inc.php
+++ week1/solution-test-jezelf-2/nav.inc.php
@@ -1,16 +1,15 @@
 
 <?php
 
-  $currentFile = $_SERVER["SCRIPT_NAME"];
-  $parts = Explode('/', $currentFile);
+  $page = basename($_SERVER['SCRIPT_NAME']);
 
  ?>
 
   <nav>
     <ul class="grid-container">
-      <li class="grid-item"><a href="index.php" class="<?php if ($parts[count($parts) - 1] == "index.php") echo "active";?>">Home</a></li>
-      <li class="grid-item"><a href="about.php" class="<?php if ($parts[count($parts) - 1] == "about.php") echo "active";?>">About</a></li>
-      <li class="grid-item"><a href="blog.php" class="<?php if ($parts[count($parts) - 1] == "blog.php") echo "active";?>">Blog</a></li>
-      <li class="grid-item"><a href="contact.php" class="<?php if ($parts[count($parts) - 1] == "contact.php") echo "active";?>">Contact</a></li>
+      <li class="grid-item"><a href="index.php" <?php if ($page == 'index.php') { echo 'class="active"';} ?> >Home</a></li>
+      <li class="grid-item"><a href="about.php" <?php if ($page == 'about.php') { echo 'class="active"';} ?> >About</a></li>
+      <li class="grid-item"><a href="blog.php" <?php if ($page == 'blog.php') { echo 'class="active"';} ?> >Blog</a></li>
+      <li class="grid-item"><a href="contact.php" <?php if ($page == 'contact.php') { echo 'class="active"';} ?> >Contact</a></li>
     </ul>
   </nav>
